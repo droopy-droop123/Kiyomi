3eb014b691d7e78d048d6143ddbceb752b5b5774
David Foster <worshipleader107@gmail.com>
2017-10-05 14:24:50 -0500

    added a command example

---
+++ commands/example.js
@@ -0,0 +1,6 @@
+module.exports = {
+     main: function(bot, msg) {
+        msg.channel.sendMessage("you can put whatever you want inside of these quotes if it's a simple command")
+    },
+    help: 'Put what you want here for the help to be'
+};
